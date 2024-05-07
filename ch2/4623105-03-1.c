1c65bf25c7a133f5836f5cfc7773bdc7ee624c31
SWKIMA-arch <greyonth@gmail.com>
2024-05-07 22:59:07 +0900

    first write of functions

--- ch2/4623105-03-1.c
+++ ch2/4623105-03-1.c
@@ -10,6 +10,12 @@ struct cell {
 };
 
 int allocate_object(struct cell *L, int *f);
+void free_object(struct cell *L, int *freeL, int *x);
+void list_insert(struct cell *L, int *head, int *p, int *x);
+int list_search(struct cell *L, int *head, int *a);
+void list_delete(struct cell *L, int *head, int *x);
+int list_maximum(struct cell *L, int *head);
+int list_minimum(struct cell *L, int *head);
 
 int main(void) {
   struct cell List[maxN];
@@ -44,4 +50,92 @@ int main(void) {
   freeL = 0;
 
   return 0;
+}
+
+int allocate_object(struct cell *L, int *f) {
+  int x;
+
+  if (*f == -1) {
+    printf("error: out of space\n");
+    x = -1;
+  }
+
+  else {
+    x = *f;
+    *f = L[*f].next;
+  }
+  return x;
+}
+
+void free_object(struct cell *L, int *freeL, int *x) {
+  L[*x].next = *freeL;
+  *freeL = *x;
+}
+
+void list_insert(struct cell *L, int *head, int *p, int *x) {
+  if (*p != -1) {
+    L[*x].prev = *p;
+    L[*x].next = L[*p].next;
+    L[*p].next = *x;
+    L[L[*x].next].prev = *x; // prev[q] = x;
+  } else {
+    L[*x].prev = *p;
+    L[*x].next = *head;
+    *head = *x;
+    L[L[*x].next].prev = *x;
+  }
+}
+
+int list_search(struct cell *L, int *head, int *a) {
+  int x = *head;
+  while (x != -1 && L[x].key != *a) {
+    x = L[x].next;
+  }
+  return x;
+}
+
+void list_delete(struct cell *L, int *head, int *x) {
+  if (L[*x].prev != -1) {
+    L[L[*x].prev].next = L[*x].next;
+    if (L[*x].next != -1) {
+      L[L[*x].next].prev = L[*x].prev;
+    }
+  } else {
+    *head = L[*x].next;
+    if (L[*x].next != -1) {
+      L[L[*x].next].prev = -1;
+    }
+  }
+}
+
+int list_maximum(struct cell *L, int *head) {
+  int x;
+  x = *head;
+  int y;
+  y = x;
+  int max = -2147483648;
+  while (L[x].key == -1) {
+    if (L[x].key > max) {
+      max = L[x].key;
+      y = x;
+    }
+    x = L[x].next;
+  }
+  return y;
+}
+
+int list_minimum(struct cell *L, int *head) {
+  int x;
+  x = *head;
+  int y;
+  y = x;
+  int min = 2147483647;
+  while (L[x].key == -1) {
+    if (L[x].key < min) {
+      min = L[x].key;
+      y = x;
+    }
+    x = L[x].next;
+  }
+  return y;
 }
